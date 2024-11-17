26c47f55ae120bf92d61796de961b2afc364b45b
chisonia <sonya12slyvia@gmail.com>
2024-11-17 17:08:49 +0100

    Pycode correction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,15 +6,17 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic authentication class."""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:  # noqa E501
         """
-        Extracts the Base64 part of the Authorization header for Basic Authentication.
+        Extracts the Base64 part of the Authorization
+        header for Basic Authentication.
 
         Args:
             authorization_header (str): The Authorization header.
 
         Returns:
-            str: The Base64 part of the Authorization header, or None if invalid.
+            str: The Base64 part of the Authorization header,
+            or None if invalid.
         """
         if authorization_header is None:
             return None

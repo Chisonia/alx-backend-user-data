3f0f0b70720eff35ae88a5ecbe5a0a6764d1a90a
chisonia <sonya12slyvia@gmail.com>
2024-11-17 17:51:04 +0100

    Decoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ BasicAuth module """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,3 +26,22 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]  # Extract the part after "Basic "
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # noqa E501
+        """
+        Decodes a Base64 string to its UTF-8 decoded value.
+
+        Args:
+            base64_authorization_header (str): The Base64 string.
+
+        Returns:
+            str: The decoded UTF-8 string if valid, otherwise None.
+        """
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):  # noqa E501
+            return None
+        try:
+            # Decode Base64 and convert bytes to string
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
